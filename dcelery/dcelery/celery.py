d353a932a18ea0cba836f264f87f106a068abec5
tarvel <taiyebabatundejames@gmail.com>
2026-09-16 17:42:24 +0100

    Remove old cworker app and add newapp with initial structure and Celery configuration

---
+++ dcelery/dcelery/celery.py
@@ -0,0 +1,19 @@
+import os
+from celery import Celery
+
+
+# used to load the settings.py file of the django project. This is necessary for Celery to access the Django settings and configurations
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings') 
+
+# initialize a new Celery application instance with the name 'dcelery'. This name is used to identify the Celery application
+app = Celery('dcelery')
+
+# load the celery configs from the django settings file, using CELERY as name space (anything that starts with CELERY_ in the settings.py file will be used as a celery config)
+app.conf.from_object('django.conf:settings', namespace='CELERY')
+
+@app.task
+def add_numbers():
+    return
+
+
+app.autodiscover_tasks() # finds all the tasks located in task.py of the installed apps (in django settings.py file) and registers them with the Celery application. This allows Celery to know about all the tasks defined in the project and be able to execute them when requested.
